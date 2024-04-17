2af75896a698e727890fdb957aa579d6a368c698
bentran1vn <tan182205@gmail.com>
2024-04-17 22:57:53 +0700

    Insert Category_id Field

--- src/routes/product.routers.ts
+++ src/routes/product.routers.ts
@@ -4,7 +4,8 @@ import databaseServices from '~/services/database.services'
 const productRouter = Router()
 
 productRouter.get('/', async (req, res) => {
-  const product = await databaseServices.products.findOne()
+  const id = req.params.id
+  const product = await databaseServices.products.findOne(id)
   console.log(product)
   res.send(product?.cover_image)
 })
