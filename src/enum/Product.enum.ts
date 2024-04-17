441b2b5f72e4c99e23b9e9d75b7098de4d592572
lamhoang24200@gmail.com <lamhoang24200@gmail.com>
2024-04-18 01:55:32 +0700

    add color

--- src/enum/Product.enum.ts
+++ src/enum/Product.enum.ts
@@ -20,9 +20,20 @@ export enum ProductSize {
 }
 
 export enum ProductColor {
-  OFFWHITE = 1,
+  OFFWHITE = 0,
   'LIGHT GRAY' = 2,
-  BLACK = 3,
-  PINK = 4,
-  KHAKI = 33
+  GRAY = 3,
+  'DARK GRAY' = 8,
+  BLACK = 9,
+  PINK = 10,
+  RED = 16,
+  ORANGE = 27,
+  NATURAL = 30,
+  BEIGE = 32,
+  KHAKI = 33,
+  'DARK BROWN' = 38,
+  YELLOW = 41,
+  OLIVE = 56,
+  BLUE = 63,
+  NAVY = 69
 }
